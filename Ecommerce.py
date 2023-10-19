ccdb97e0f24df0d4b9fdf63b61bdff78314c0a1d
Takina4445 <131225876+Takina4445@users.noreply.github.com>
2023-10-19 22:36:30 +0800

    新增答題正確率計算

--- Ecommerce.py
+++ Ecommerce.py
@@ -2,10 +2,12 @@ import pandas as pd
 import random
 df=pd.read_excel("TQC-電商考題.xlsx",sheet_name="工作表2",usecols="C:I,K")
 random.seed()
+userTotalCorrectAnswer=0
+userTotalAnswer=0
 while True:
+    userTotalAnswer+=1#使用者答題總數+1
     dfRandomChoiceColum=random.randint(1,df.shape[0])#0是欄位名稱
     dfGetQuestionNumber=df.iat[dfRandomChoiceColum,0]
-    print(dfRandomChoiceColum+2)
     dfGetQuestionAnswer=df.iat[dfRandomChoiceColum,1]#答案
     dfGetQuestionTopic=df.iat[dfRandomChoiceColum,2]
     dfGetQuestionOption1=df.iat[dfRandomChoiceColum,3]
@@ -23,7 +25,9 @@ while True:
     print(f"4.{dfGetQuestionOption4}")
     userInputAnswer=input("請作答:")
     if userInputAnswer==str(dfGetQuestionAnswer):
-        print("答案正確!!!")
+        userTotalCorrectAnswer+=1#使用者正確答題數量+1
+        print("答題正確!!!")
     else:
         print(f"錯誤答案，正確答案為:{dfGetQuestionAnswer}")
-    print("*"*20)
+    
+    print("*"*20+f"({userTotalCorrectAnswer}/{userTotalAnswer}，正確率:{userTotalCorrectAnswer/userTotalAnswer*100}%)\n")
